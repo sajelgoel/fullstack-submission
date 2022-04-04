8e2c3049ad9cbd53e984bfe9dffc3c09d72bd558
sajelgoel <sajel_goel@yahoo.com>
2022-04-04 22:01:50 +0530

    phonebook exercise changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,71 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Filter from "./Filter";
+import PersonForm from "./PersonForm";
+
 const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const changeName = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const changeNumber = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const submitForm = (event) => {
+    event.preventDefault();
+    const present = persons.filter((val) => val.name === newName);
+    if (present.length === 0) {
+      setPersons(persons.concat([{ name: newName, number: newNumber }]));
+    } else {
+      alert(`${newName} is already added to phonebook`);
+    }
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const changeFilter = (event) => {
+    setFilter(event.target.value);
+  };
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
+
+  const contacts = persons
+    .filter((value) => {
+      return value.name.toLowerCase().includes(filter.toLowerCase());
+    })
+    .map((val, index) => <Person key={index} val={val} />);
   return (
     <div>
-      Hello world
+      <h2>Phonebook</h2>
+      <Filter filter={filter} handleChange={changeFilter} />
+      <h3>add a new</h3>
+      <PersonForm
+        handleSubmit={submitForm}
+        newName={newName}
+        changeName={changeName}
+        newNumber={newNumber}
+        changeNumber={changeNumber}
+      />
+      <h3>Numbers</h3>
+      {contacts}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
+const Person = ({ val }) => (
+  <p>
+    {val.name} {val.number}
+  </p>
+);
---
+++ src/PersonForm.js
@@ -0,0 +1,17 @@
+const PersonForm = (props)=>{
+return (
+    <form onSubmit={props.handleSubmit}>
+        <div>
+          name: <input value={props.newName} onChange={props.changeName} />
+        </div>
+        <div>
+          number: <input value={props.newNumber} onChange={props.changeNumber} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+)
+}
+
+export default PersonForm;
